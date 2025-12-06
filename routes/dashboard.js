c23325142b2cb585b7dcc81cae67f3f430f50dcd
FadeeAttieh <fadee_01@hotmail.com>
2025-12-06 05:56:57 +0000

    Fix JWT middleware export and add userId to request

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const dashboardController = require('../controllers/dashboardController');
-const { verifyToken } = require('../middleware/authJwt');
+const verifyToken = require('../middleware/authJwt');
 
 // Get dashboard statistics grouped by currency
 router.get('/stats', verifyToken, dashboardController.getDashboardStats);
